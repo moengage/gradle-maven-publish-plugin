5d3a6bc2347d67d805b7d09edfdcae69b4c14b23
abhishek-engg-mobilesdk <abhishek.kumar@moengage.com>
2024-10-18 17:32:09 +0530

    MOEN-34834: Release Script Fixing For Assemble

--- .github/scripts/release.main.kts
+++ .github/scripts/release.main.kts
@@ -8,6 +8,10 @@ val publishKey = args[0]
 val secretKey = args[1]
 
 mergeDevBranchToMaster()
+if (executeCommandOnShell("./gradlew assemble --stacktrace") != 0) {
+    println("::error::Failed to publish plugin")
+    exitProcess(1)
+}
 if (executeCommandOnShell("./gradlew publishPlugins -Pgradle.publish.key=$publishKey -Pgradle.publish.secret=$secretKey") != 0) {
     println("::error::Failed to publish plugin")
     exitProcess(1)
